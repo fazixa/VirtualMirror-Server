a2030162b8fe28b345633afdb407b75978be6ac9
Ali Goldani <ali.goldani@ymail.com>
2021-05-17 01:04:03 +0430

    test code

--- test.py
+++ test.py
@@ -2,11 +2,12 @@ import cv2
 import src.tints.cv.makeup.utils as mutils
 
 mutils.start_cam()
-# mutils.enable_makeup('eyeshadow', 200, 10, 20, 1)
-# mutils.enable_makeup('blush', 130, 197, 81, .6)
-mutils.enable_makeup('lipstick', 255, 0, 0, gloss=True)
+mutils.enable_makeup('eyeshadow', 34, 74, 162, .7)
+mutils.enable_makeup('blush', 87, 36, 51, .5)
+mutils.enable_makeup('eyeliner', 142, 30, 29, 1)
+mutils.enable_makeup('lipstick', 142, 30, 29, gloss=True, lipstick_type=None)
 # mutils.enable_makeup('concealer', 200, 10, 20)
-mutils.enable_makeup('foundation', r=68, g=59, b=49)
+# mutils.enable_makeup('foundation', 255, 253, 208)
 while True:
     cv2.imshow("Frame", mutils.apply_makeup_video())
     key = cv2.waitKey(1) & 0xFF
